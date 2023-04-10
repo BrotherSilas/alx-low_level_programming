94ac7384c0206c9add43e7c51c6196c00f870860
BrotherSilas <silasedetsnr@gmail.com>
2023-04-10 16:38:48 -0700

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,12 +22,12 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	w = write(fp, text_content, len);
 
 	if (fp == -1 || w == -1)
 		return (-1);
 
-	close(fd);
+	close(fp);
 
 	return (1);
 }
