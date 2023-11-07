82bef948f3cffd40d66ddfd1980859cbced6a87d
root <silasedetsnr@gmail.com>
2023-11-07 09:27:03 -0800

    file io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,23 +13,21 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t nrd, nwr;
 	char *buf;
-	ssize_t fd;
-	ssize_t ab;
-	ssize_t ba;
 
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * (letters);
+	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
-	ab = read(fd, buf, letters);
-	ba = write(STDOUT_FILENO, buf, ab);
-	free(buf);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 	close(fd);
-	return (ba);
+	free(buf);
+	return (nwr);
 }
