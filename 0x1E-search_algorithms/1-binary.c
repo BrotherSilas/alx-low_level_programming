3fe5760fd8a6db3d8cbc89779f3c6d635d70f17b
BrotherSilas <silasedetsnr@gmail.com>
2024-07-18 06:46:58 -0700

    java

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,38 +17,46 @@ void display_array(int *array, size_t left, size_t right);
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t middle;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t middle;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+	return (-1);
 
-    while (left <= right)
-    {
+	while (left <= right)
+	{
 	display_array(array, left, right);
 	middle = left + (right - left) / 2;
 
-        if (array[middle] == value)
+	if (array[middle] == value)
 		return (middle);
 	else if (array[middle] < value)
 		left = middle + 1;
 	else
 		right = middle - 1;
-    }
-    return (-1);
+	}
+	return (-1);
 }
 
+/**
+ * display_array - function to print the array being searched
+ * every time it changes
+ * @array: Pointer to the first element of the array to search in
+ * @left: first index of the array/subarray
+ * @right: last index of the array/subarray
+ */
 void display_array(int *array, size_t left, size_t right)
 {
 	size_t i;
+
 	printf("Searching in array: ");
 	for (i = left; i <= right; i++)
 	{
 		printf("%d", array[i]);
 		if (i < right)
 			printf(", ");
-    	}
+	}
 
-    	printf("\n");
+	printf("\n");
 }
